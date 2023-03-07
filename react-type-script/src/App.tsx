4598304dc901d10afb685bf9a14fbcd80ee8129d
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-03-07 13:56:01 +0600

    props implemented

--- react-type-script/src/App.tsx
+++ react-type-script/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greet from './component/Greet';
 
 function App() {
   return (
     <div className="App">
-     
+      <Greet name='Rakib' />
     </div>
   );
 }
